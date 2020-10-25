f15a23124b97f770c5adca8423409cc5e0d3222d
caixxiong <caixionggle@gmail.com>
2020-10-25 17:32:14 +0800

    add index.html

--- codes/utils/utils/ce_loss_util.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-
-def ce_loss(soft_labels, logits):
-    probs = tf.clip_by_value(tf.nn.softmax(logits, axis=-1), 1e-10, 10)
-    ce = -tf.reduce_mean(tf.reduce_sum(soft_labels * tf.log(probs), axis=-1))
-    return ce
